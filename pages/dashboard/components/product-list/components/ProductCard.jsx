7fcfc062ecdce886aa1371abc95a3c9d3ba8c0bc
DECode.studio <decode.std@gmail.com>
2022-06-16 23:30:13 +0700

    add shadow

--- pages/dashboard/components/product-list/components/ProductCard.jsx
+++ pages/dashboard/components/product-list/components/ProductCard.jsx
@@ -3,8 +3,8 @@ import currencyFormat from "../../../service/currency";
 const ProductCard = ({ productImage, productName, productCategory, productPrice }) => {
     return (
         <div className="col-6">
-            <div className="card h-100">
-                <img src={productImage} className="card-img-top" alt="..." />
+            <div className="card h-100 br-10 shadow">
+                <img src={productImage} className="card-img-top br-10" alt="..." />
                 <div className="card-body">
                     <h6 className="card-title">{productName}</h6>
                     <p className="card-text">{productCategory}</p>
