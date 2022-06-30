3044b572e0f6b1c93ffac8cd29ba89fb6cf31281
zildanIsr <64051285+zildanIsr@users.noreply.github.com>
2022-06-30 19:55:50 +0700

    zildan: fix navbar error

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -2,16 +2,16 @@ import Logo from "./components/logo";
 import SearchEngine from "./components/SearchEngine";
 import LoginButton from "./components/LoginButton";
 import IconButton from "./components/Icon";
-import { useEffect, } from "react";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
 
-    const token = '';
+    const [isLoggedIn, setIsLoggedIn] = useState([]);
 
-    if (typeof window !== 'undefined') {
-        // Perform localStorage action
-        token = localStorage.getItem('token')
-    }
+    useEffect(function() {
+       const token = localStorage.getItem('token')
+       setIsLoggedIn(token)
+    },[]);
 
     return (
         <nav className="navbar navbar-expand-lg bg-white shadow-sm p-3 mb-lg-5 mb-3 bg-body rounded">
@@ -21,7 +21,7 @@ const NavBar = () => {
                 </div>
                 <SearchEngine />
                     <ul className="navbar-nav ms-md-auto flex-sm-row">
-                        {!token ? 
+                        {!isLoggedIn ? 
                             <li className="nav-item">
                                 <LoginButton />    
                             </li>
