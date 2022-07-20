27fe060f30bb7d544f86e3695ba36e0a1c3dbf13
DECode.studio <decode.std@gmail.com>
2022-07-20 08:16:05 +0700

    change load image animation

--- pages/product/add/components/loading-box/LoadingBox.jsx
+++ pages/product/add/components/loading-box/LoadingBox.jsx
@@ -1,9 +1,12 @@
+import { LoadingAnimation } from "../../../../../components";
+
 const LoadingBox = () => {
     return (
         <div className="col-2">
-            <div className="btn position-relative border border-2 border-dark br-10" style={{ width: "90px", height: "90px" }}>
+            <div className="btn position-relative br-10" style={{ width: "90px", height: "90px" }}>
                 <div className="position-absolute top-50 start-50 translate-middle">
-                    <div className="col-2">Loading...</div>
+                    {/* <div className="col-2">Loading...</div> */}
+                    <LoadingAnimation />
                 </div>
             </div>
         </div>
